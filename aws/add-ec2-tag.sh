2ec933705e13013870a006e90cc23b847a712404
Chris Meader <cmeader1053@gmail.com>
2026-01-29 17:13:31 -0600

    Update add-ec2-tag.sh

--- aws/add-ec2-tag.sh
+++ aws/add-ec2-tag.sh
@@ -1,3 +1,5 @@
+# This bash script can be ran in the aws cli to add a tag key and value to ALL ec2 instances
+
 aws ec2 create-tags \
   --resources $(aws ec2 describe-instances \
     --query "Reservations[].Instances[].InstanceId" \
