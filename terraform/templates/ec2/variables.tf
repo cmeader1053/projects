1d16c120b00c86cc4d8119d850488a930a5aca3b
Chris Meader <cmeader1053@gmail.com>
2026-04-13 15:56:00 -0500

    Add EC2 Terraform variables definition file
    This file contains variable definitions for EC2 instance deployment, including AMI ID, instance type, subnet ID, security group ID, SSH key pair, IAM profile, root volume size, instance name, and tags.

---
+++ terraform/templates/ec2/variables.tf
@@ -0,0 +1,64 @@
+/*
+Title = EC2 Terraform Variables
+Description = Variables Terraform document for EC2 Instance deployment. 
+Directions = Update the variables as seen fit to be called by the main.tf file.
+*/
+
+# Instance AMI
+variable "ami_id" {
+	description = "AMI ID for EC2 instance"
+	type = string
+}
+
+# Define Instance Type
+variable "instance_type" {
+	description = "Instance type for EC2 instance"
+	type = string
+	default = "t3.micro"
+}
+
+# Subnets
+variable "subnet_id" {
+	description = "Subnet ID for EC2 instance"
+	type = string
+}
+
+# Add Security Groups
+variable "sec_grp_id" {
+	description = "Security group ID for EC2 instance"
+	type = list(string)
+}
+
+# Add SSH Key Pair
+variable "key_name" {
+	description = "SSH key pair for EC2 instance"
+	type = string
+	default = null
+}
+
+# Add IAM Profile
+variable "iam_profile" {
+	description = "IAM profile attached to EC2 instance"
+	type = string
+	default = null
+}
+
+# Define EBS Root Volume
+variable "root_vol_size" {
+	description = "Root EBS volume size in GB"
+	type = number
+	default = 20
+}
+
+# EC2 Instance Name
+variable "name" {
+	description = "Name of EC2 instance"
+	type = string
+}
+
+# Tags		# Tags can be modified in the terraform.tfvars file
+variable "tags" {
+	description = "Tags applied to all resources"
+	type = map(string)
+	default = {}
+}
