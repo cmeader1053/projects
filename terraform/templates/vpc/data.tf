d4e12c6e5ac1869ed74ea166a3658d78cb8b98df
Chris Meader <cmeader1053@gmail.com>
2026-04-17 12:11:56 -0500

    Add data source for current AWS region
    Added data source to retrieve current AWS region.

--- terraform/templates/vpc/data.tf
+++ terraform/templates/vpc/data.tf
@@ -3,6 +3,10 @@ Title        = VPC Data Terraform
 Description  = Data file to pull available AZ's in region to verify AZ specified exist
 */
 
+# Get Current AWS Region
+data "aws_region" "current" {}
+
+# Check for available AZ's in current AWS Region
 data "aws_availability_zones" "available" {
   state = "available"
 }
